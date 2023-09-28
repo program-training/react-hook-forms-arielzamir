348a05826e52d398e0f33f2b1be29b62e6f8df79
Ariel Zamir <arielzamir100@gmail.com>
2023-09-28 13:04:16 +0300

    Added exercise 1.

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,65 +1,81 @@
-import  { useState, ChangeEvent, FormEvent } from 'react';
-
-interface FormData {
-  username: string;
-  email: string;
-  password:string;
-}
+import { useForm, FieldValues } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 function RegularForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    email: '',
-    password: '',
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert(JSON.stringify(formData));
+  const onSubmit = (data: FieldValues) => {
+    alert(JSON.stringify(data));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Change Me To React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Change Me To React Hook Form</h1>
       <div>
         <input
+          {...register("username", {
+            required: "Username is required",
+            minLength: {
+              value: 2,
+              message: "Username must be at least 2 characters.",
+            },
+          })}
           type="text"
           id="username"
           name="username"
-          placeholder='Enter UserName'
-          value={formData.username}
-          onChange={handleChange}
+          placeholder="Enter UserName"
         />
       </div>
+      <ErrorMessage errors={errors} name="username" />
       <div>
         <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Enter a valid email address",
+            },
+          })}
           type="text"
           id="email"
           name="email"
-          placeholder='Enter Email'
-          value={formData.email}
-          onChange={handleChange}
+          placeholder="Enter Email"
         />
       </div>
+      <ErrorMessage errors={errors} name="email" />
       <div>
         <input
-          type="text"
+          {...register("password", {
+            required: "Password is required",
+            pattern: {
+              value:
+                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]{8,}$/,
+              message:
+                "Password must contain a number, one uppercase letter, one lowercase letter, and one special character.",
+            },
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters.",
+            },
+            maxLength: {
+              value: 20,
+              message: "Password must not exceed 20 characters.",
+            },
+          })}
+          type="password"
           id="password"
           name="password"
-          placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
+          placeholder="Enter Password"
         />
       </div>
-      <button type="submit">Submit</button>
+      <ErrorMessage errors={errors} name="password" />
+      <div>
+        <button type="submit">Submit</button>
+      </div>
     </form>
   );
 }
